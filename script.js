366db17aa89264a7a582165251a6ae3ca856e504
faustfino <119843184+faustfino@users.noreply.github.com>
2023-12-12 15:22:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+const users = [
+  { email: "mail@mail.dk", password: "user123" },
+  { email: "bob@mail.dk", password: "bob123" },
+  { email: "hej@mail.dk", password: "hej123" },
+];
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const email = loginForm.email.value;
     const password = loginForm.password.value;
 
-    if (email === "mail@mail.dk" && password === "user123") {
+    const user = users.find(u => u.email === email && u.password === password);
+
+    if (user) {
         window.location.href = "pages/temperature.html";
     } else {
         loginErrorMsg.style.opacity = 1;
