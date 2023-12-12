cc8a93906316d01ab054760d281548e8e7045b8a
Camipojep <125866708+Camipojep@users.noreply.github.com>
2023-12-12 22:57:24 +0100

    dropdown

--- script.js
+++ script.js
@@ -2,22 +2,51 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-const users = [
-  { email: "mail@mail.dk", password: "user123" },
-  { email: "bob@mail.dk", password: "bob123" },
-  { email: "hej@mail.dk", password: "hej123" },
-];
-
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const email = loginForm.email.value;
     const password = loginForm.password.value;
 
-    const user = users.find(u => u.email === email && u.password === password);
-
-    if (user) {
+    if (email === "mail@mail.dk" && password === "user123") {
         window.location.href = "pages/temperature.html";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
 })
+
+
+function dropdown() {
+    document.getElementById("dropdown").classList.toggle("show");
+  }
+  
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn-graf')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function dropdown() {
+    document.getElementById("dropdown").classList.toggle("show");
+  }
+  
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn-profil')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
